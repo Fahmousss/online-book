0d3a4c43b201d23136884eedb332d506bbdf9d1c
Fahmousss <09031382227177@student.unsri.ac.id>
2024-11-05 21:02:38 +0700

    add model and migration

---
+++ app/Models/Book.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Book extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'author_id',
+        'category_id',
+        'stock',
+        'price',
+        'published_date',
+        'images',
+        'is_featured',
+    ];
+
+    protected $dates = ['deleted_at', 'published_date'];
+
+    // Relationship with Author
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    // Relationship with Category
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relationship with OrderItems
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItems::class);
+    }
+}
