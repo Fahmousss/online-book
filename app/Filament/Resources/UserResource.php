204c5411f1b52a84a08268b7df19662af4c9f6cb
Fahmousss <09031382227177@student.unsri.ac.id>
2024-11-27 00:43:12 +0700

    Fix Resources

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -41,7 +41,7 @@ class UserResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
@@ -62,8 +62,8 @@ class UserResource extends Resource
     {
         return [
             'index' => Pages\ListUsers::route('/'),
-            'create' => Pages\CreateUser::route('/create'),
-            'edit' => Pages\EditUser::route('/{record}/edit'),
+            // 'create' => Pages\CreateUser::route('/create'),
+            // 'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
 }
